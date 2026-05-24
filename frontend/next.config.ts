5d112fe15691400b6f8241963981e42c14b782db
Immaculate Munde <immaculatemunde1@gmail.com>
2026-05-24 22:26:04 +0300

    Remove custom webpack configuration from Next.js setup and add a new script for HTTP development server in package.json.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import webpack from "webpack";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@creit.tech/stellar-wallets-kit"],
@@ -7,18 +6,6 @@ const nextConfig: NextConfig = {
     resolveAlias: {
       buffer: "buffer",
     },
-  },
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      buffer: require.resolve("buffer/"),
-    };
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ["buffer", "Buffer"],
-      })
-    );
-    return config;
   },
 };
 
